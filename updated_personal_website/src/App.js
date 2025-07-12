b110e29de515c98c42386191ca81ea8211b43ada
Mihalis Koutouvos <mihaliskoutouvos@gmail.com>
2025-07-12 14:22:10 -0400

    Fixed various imports in App.ks

--- updated_personal_website/src/App.js
+++ updated_personal_website/src/App.js
@@ -1,6 +1,11 @@
 import './App.css';
 import React from "react";
 import Navbar from "./Navbar";
+import About from "./About";
+import Skills from ".Skills";
+import Projects from ".Projects";
+import Interests from ".Interests";
+import Contact from ".Contact";
 
 function App() {
   return (
