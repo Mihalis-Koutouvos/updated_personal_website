4a78f0491d92a0db95094329c3f3f17104c9de30
Mihalis Koutouvos <mihaliskoutouvos@gmail.com>
2025-07-12 14:33:47 -0400

    Started outlining the navbar

--- updated_personal_website/src/Navbar.js
+++ updated_personal_website/src/Navbar.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
+import "./Navbar.css"
 
 const Navbar = () => {
+
+    //Creating handlers for clicking on the navbar
+    //click is set to false, and setClick will be used to change the 
+    //boolean expression
+    const [click, setClick] = useState(false)
+    //This is the function to change the click boolean
+    const handleClick = () => setClick(!click)
+
+    const closeMenu = () => setClick(false)
+
+
+
+
   return (
-    <div>Navbar</div>
+    <div className="header">
+        <nav className="navbar">
+            <div className="nav-left"> </div>
+            <div className="nav-center"> </div>
+            <div className="nav-right"> </div>
+        </nav>
+
+    </div>
   )
+
+
+
 }
 
 export default Navbar
