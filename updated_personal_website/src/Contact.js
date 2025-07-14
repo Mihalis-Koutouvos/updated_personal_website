cbf8125535a410fe9e0d1ef5d2ba1cf0a79d769b
Mihalis Koutouvos <mihaliskoutouvos@gmail.com>
2025-07-14 13:18:43 -0400

    Added a section for Contact

--- updated_personal_website/src/Contact.js
+++ updated_personal_website/src/Contact.js
@@ -2,7 +2,19 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    //id necessary for linking when we use hashtags
+    //Child error with image, so changed to inline
+    <div className='contact' id='contact'>
+        <div className='container'>
+        
+            <div className='text-section'>
+                <h2>Contact:</h2>
+                <span className='highlight-line'></span>
+                
+                
+            </div>
+        </div>
+    </div>
   )
 }
 
