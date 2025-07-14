76556f78bc4b857a923b4b5b8e64809f4decb534
Mihalis Koutouvos <mihaliskoutouvos@gmail.com>
2025-07-14 13:15:21 -0400

    Added section for Skills

--- updated_personal_website/src/Skills.js
+++ updated_personal_website/src/Skills.js
@@ -2,7 +2,19 @@ import React from 'react'
 
 const Skills = () => {
   return (
-    <div>Skills</div>
+    //id necessary for linking when we use hashtags
+    //Child error with image, so changed to inline
+    <div className='skills' id='skills'>
+        <div className='container'>
+        
+            <div className='text-section'>
+                <h2>Skills:</h2>
+                <span className='highlight-line'></span>
+                
+                
+            </div>
+        </div>
+    </div>
   )
 }
 
