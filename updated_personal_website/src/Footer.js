a1c63bcc5c0df0060c6718319dd7dbb706f565e9
Mihalis Koutouvos <mihaliskoutouvos@gmail.com>
2025-07-14 13:27:56 -0400

    Synced up the remaining CSS files to their corresponding JS files

--- updated_personal_website/src/Footer.js
+++ updated_personal_website/src/Footer.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import './Footer.css';
 //rafce
 
 const Footer = () => {
--- updated_personal_website/src/Interests.js
+++ updated_personal_website/src/Interests.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import './Interests.css';
 
 const Interests = () => {
   return (
