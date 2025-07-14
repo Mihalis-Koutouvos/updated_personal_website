cbd7318002300297a977cbb2ff64855e7f729ba9
Mihalis Koutouvos <mihaliskoutouvos@gmail.com>
2025-07-14 13:15:55 -0400

    Added a section for Interests

--- updated_personal_website/src/Interests.js
+++ updated_personal_website/src/Interests.js
@@ -2,7 +2,19 @@ import React from 'react'
 
 const Interests = () => {
   return (
-    <div>Interests</div>
+    //id necessary for linking when we use hashtags
+    //Child error with image, so changed to inline
+    <div className='interests' id='interests'>
+        <div className='container'>
+        
+            <div className='text-section'>
+                <h2>Interests:</h2>
+                <span className='highlight-line'></span>
+                
+                
+            </div>
+        </div>
+    </div>
   )
 }
 
