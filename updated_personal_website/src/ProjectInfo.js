04f68381a1ebc86bc764a4b8a6d56dd6be139674
Mihalis Koutouvos <mihaliskoutouvos@gmail.com>
2025-07-26 19:56:51 -0400

    Added additional project files to create card components

---
+++ updated_personal_website/src/ProjectInfo.js
@@ -0,0 +1,33 @@
+const ProjectInfo = [
+    {
+        id: 1,
+        name: "Updated Portfolio",
+        description: "An updated version of my original portfolio that utilizes more modern coding techniques.",
+        link: "https://github.com/Mihalis-Koutouvos/updated_personal_website",
+        tags: ['React.js', ],
+    },
+    {
+        id: 2,
+        name: "MetisCare Hub",
+        description: "A hospital-based, full-stack, ETL pipeline that speeds up the process of finding vacant rooms during stressful hours on call.",
+        link: "https://github.com/Mihalis-Koutouvos/MetisCare_Hub",
+        tags: ['S3', 'Lambda', 'EC2', 'React', 'Python', 'Flask', 'MySQL'],
+    },
+    {
+        id: 3,
+        name: "Odessa",
+        description: "An AI chatbot that educates users on common physical injuries and diseases.",
+        link: "https://github.com/Mihalis-Koutouvos/odessa",
+        tags: ['Python', 'PyTorch', 'Miniconda', 'JSON'],
+    },
+    {
+        id: 4,
+        name: "Portfolio - Original",
+        description: "My first attempt at launching a personal portfolio to display projects, achievements, skills, and hobbies.",
+        link: "https://github.com/Mihalis-Koutouvos/Mihalis-Koutouvos.github.io",
+        tags: ["HTML", "CSS", "JavaScript"],
+    }
+
+]
+
+export default ProjectInfo;
