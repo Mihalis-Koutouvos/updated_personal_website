edfdc4c71e170ecb0c1d0a540ef38cf4e2668895
Mihalis Koutouvos <mihaliskoutouvos@gmail.com>
2025-08-07 13:22:20 -0400

    Added experiences code and need to fix some imports

---
+++ updated_personal_website/src/ExperienceInfo.js
@@ -0,0 +1,201 @@
+const ExperienceInfo = [
+    {
+        title: "Lifeguard",
+        company_name: "Life Time",
+        icon: ,
+        iconBg,
+        date: "",
+        points: [
+            "",
+            "",
+            "",
+
+        ],
+    },
+
+    {
+        title: "Summer Swim Team Head Coach",
+        company_name: "Life Time",
+        icon: ,
+        iconBg,
+        date: "",
+        points: [
+            "",
+            "",
+            "",
+
+        ],
+    },
+
+    {
+        title: "Assistant Swim Team Coach",
+        company_name: "Life Time",
+        icon: ,
+        iconBg,
+        date: "",
+        points: [
+            "",
+            "",
+            "",
+
+        ],
+    },
+
+    {
+        title: "Aquatics Swim Instructor",
+        company_name: "Life Time",
+        icon: ,
+        iconBg,
+        date: "",
+        points: [
+            "",
+            "",
+            "",
+
+        ],
+    },
+
+    {
+        title: "Software Developer",
+        company_name: "Oasis",
+        icon: ,
+        iconBg,
+        date: "",
+        points: [
+            "",
+            "",
+            "",
+
+        ],
+    },
+
+    {
+        title: "Online Tutor",
+        company_name: "The Princeton Review",
+        icon: ,
+        iconBg,
+        date: "",
+        points: [
+            "",
+            "",
+            "",
+
+        ],
+    },
+
+    {
+        title: "Content Creator/Writer for the Marketing Team",
+        company_name: "AI NU",
+        icon: ,
+        iconBg,
+        date: "",
+        points: [
+            "",
+            "",
+            "",
+
+        ],
+    },
+
+    {
+        title: "Student Project Manager for EAI Digest",
+        company_name: "Institute for Experiential AI at Northeastern University",
+        icon: ,
+        iconBg,
+        date: "",
+        points: [
+            "",
+            "",
+            "",
+
+        ],
+    },
+
+    {
+        title: "Beginner Russian Ambassador",
+        company_name: "NUCALLS",
+        icon: ,
+        iconBg,
+        date: "",
+        points: [
+            "",
+            "",
+            "",
+
+        ],
+    },
+
+    {
+        title: "Research Assistant",
+        company_name: "AI Perception Lab",
+        icon: ,
+        iconBg,
+        date: "",
+        points: [
+            "",
+            "",
+            "",
+
+        ],
+    },
+
+    {
+        title: "Co-Founder/Director of Operations",
+        company_name: "MedCS Lab",
+        icon: ,
+        iconBg,
+        date: "",
+        points: [
+            "",
+            "",
+            "",
+
+        ],
+    },
+
+    {
+        title: "Teaching Assistant",
+        company_name: "Khoury College of Computer Sciences",
+        icon: ,
+        iconBg,
+        date: "",
+        points: [
+            "",
+            "",
+            "",
+
+        ],
+    },
+
+    {
+        title: "IT Application Business Analyst Developer Co-op",
+        company_name: "MassDOT",
+        icon: ,
+        iconBg,
+        date: "",
+        points: [
+            "",
+            "",
+            "",
+
+        ],
+    },
+
+    {
+        title: "Software Engineer",
+        company_name: "ScribeSense - The Future of Clinical Documentation",
+        icon: ,
+        iconBg,
+        date: "",
+        points: [
+            "",
+            "",
+            "",
+
+        ],
+    },
+
+
+]
+
+export default ExperienceInfo;
--- updated_personal_website/src/Experiences.jsx
+++ updated_personal_website/src/Experiences.jsx
@@ -1,5 +1,13 @@
 import React from 'react'
 import './Experiences.css'
+import {VerticalTimeline, VarticalTimeLineElement} from 'react-vertical-timeline-component';
+import {motion} from 'framer-motion';
+
+import 'react-vertical-timeline-component/style.min.css';
+import {styles} from './styles';
+import 
+import {SectionWrapper}
+import {textVariant} from 
 
 const Experiences = () => {
   return (
@@ -12,6 +20,8 @@ const Experiences = () => {
             <div className='text-section'>
                 <h2>Experiences:</h2>
                 <span className='highlight-line'></span>
+
+
                 
             </div>
         </div>
