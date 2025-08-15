e61ce47904e2ead0a05cfe311c635f164b7427df
Mihalis Koutouvos <mihaliskoutouvos@gmail.com>
2025-08-14 20:02:08 -0400

    Finished ExperienceInfo. Can now proceed to component creation

--- updated_personal_website/src/ExperienceInfo.js
+++ updated_personal_website/src/ExperienceInfo.js
@@ -1,16 +1,16 @@
-import * as expIcons from "./ExperSymbols";
+import * as expInfo from "./ExperSymbols";
 
 const ExperienceInfo = [
     {
         title: "Lifeguard",
         company_name: "Life Time",
-        icon: ,
+        icon: expInfo.lifeTime,
         icon_background: "#383E56",
-        date: "",
+        date: "Aug 2021 - Sep 2023",
         points: [
-            "",
-            "",
-            "",
+            "Performed surveillance over multiple pools for patrons and conducted cleaning checks across the indoor and outdoor facilities to ensure safety and customer satisfaction",
+            "Operated with chemical solutions to handle chlorine checks",
+            "Managed patron queries attentively to boost member camaraderie",
 
         ],
     },
@@ -18,41 +18,39 @@ const ExperienceInfo = [
     {
         title: "Summer Swim Team Head Coach",
         company_name: "Life Time",
-        icon: ,
+        icon: expInfo.lifeTime,
         icon_background: "#383E56",
-        date: "",
+        date: "Apr 2024 - Sep 2024",
         points: [
-            "",
-            "",
-            "",
-
+            "Raised over $11,000 for Aquatics Department via club promotion in four months",
+            "Conducted practice for age groups between 5- and 18-years-old and provide constructive feedback to improve swimmer competency",
+            "Collaborated with other swim coaches to host diving clinics with local Life Time facilities in the area",
         ],
     },
 
     {
         title: "Assistant Swim Team Coach",
         company_name: "Life Time",
-        icon: ,
+        icon: expInfo.lifeTime,
         icon_background: "#383E56",
-        date: "",
+        date: "Sep 2022 - Present",
         points: [
-            "",
-            "",
-            "",
-
+            "Raised over $20,000 for Aquatics Department via club promotion in four months",
+            "Conducting practice for age groups between 5- and 18-years-old and provide constructive feedback to improve swimmer competency",
+            "Collaborating with other swim coaches to host diving clinics with local Life Time facilities in the area",
         ],
     },
 
     {
         title: "Aquatics Swim Instructor",
         company_name: "Life Time",
-        icon: ,
+        icon: expInfo.lifeTime,
         icon_background: "#383E56",
-        date: "",
+        date: "Jun 2025 - Present",
         points: [
-            "",
-            "",
-            "",
+            "Providing swimming instruction to swimmers between 3 months and up",
+            "Enhancing learning experience by applying real-world advice to older swimmers",
+            "Teaching techniques including arm positioning in freestyle and more",
 
         ],
     },
@@ -60,23 +58,21 @@ const ExperienceInfo = [
     {
         title: "Software Developer",
         company_name: "Oasis",
-        icon: ,
+        icon: expInfo.oasis,
         icon_background: "#383E56",
-        date: "",
+        date: "Jan 2024 - Apr 2024",
         points: [
-            "",
-            "",
-            "",
-
+            "Collaborated with three other individuals to create a grade/semester calculator for college students that is more user-friendly and smoother to navigate than other calculators",
+            "Calculator worked with assignment, class, and semester-wide grades",
         ],
     },
 
     {
         title: "Online Tutor",
         company_name: "The Princeton Review",
-        icon: ,
+        icon: expInfo.princetonRev,
         icon_background: "#383E56",
-        date: "",
+        date: "May 2024 - Aug 2024",
         points: [
             "",
             "",
@@ -88,55 +84,49 @@ const ExperienceInfo = [
     {
         title: "Content Creator/Writer for the Marketing Team",
         company_name: "AI NU",
-        icon: ,
+        icon: expInfo.aiNU,
         icon_background: "#383E56",
-        date: "",
+        date: "Nov 2023 - Jan 2025",
         points: [
-            "",
-            "",
-            "",
-
+            "Operated as a remote tutor for The Princeton Review and its Tutor.com branch",
+            "Taught students ranging from middle school to college in one-on-one or group environments to improve comprehension in topics such as high school English and college-essay writing",
         ],
     },
 
     {
         title: "Student Project Manager for EAI Digest",
         company_name: "Institute for Experiential AI at Northeastern University",
-        icon: ,
+        icon: expInfo.ieaiNU,
         icon_background: "#383E56",
-        date: "",
+        date: "Oct 2024 - Apr 2025",
         points: [
-            "",
-            "",
-            "",
-
+            "Spearheaded a group of 7 students in procuring and analyzing research articles and news highlights on brand new AI updates in today’s world to present to executives at Northeastern University via a digest",
+            "Guided students by evaluating the validity of certain articles to arrange cardinal sources for the final issue",
         ],
     },
 
     {
         title: "Beginner Russian Ambassador",
         company_name: "NUCALLS",
-        icon: ,
+        icon: expInfo.nuCalls,
         icon_background: "#383E56",
-        date: "",
+        date: "Sep 2024 - Present",
         points: [
-            "",
-            "",
-            "",
-
+            "Mentoring over 12 students in learning Russian with weekly meetings and slideshows to improve Russian competency",
+            "Marshalling groups of students to local Bazaars to permit them to practice Russian",
         ],
     },
 
     {
         title: "Research Assistant",
         company_name: "AI Perception Lab",
-        icon: ,
+        icon: expInfo.aiPerceptionLab,
         icon_background: "#383E56",
-        date: "",
+        date: "Nov 2024 - Present",
         points: [
-            "",
-            "",
-            "",
+            "Research assistant for the AI Perception Lab at Northeastern University",
+            "Examining studies and data regarding novel approaches to semantic segmentation using causal inference and knowledge graphs under the supervision and guidance of Dr. Ennio Mingolla",
+            "Fabricating conversational AI models that integrate uncertainty estimation and reasoning to effectively handle ambiguity and incomplete information",
 
         ],
     },
@@ -144,13 +134,13 @@ const ExperienceInfo = [
     {
         title: "Co-Founder/Director of Operations",
         company_name: "MedCS Lab",
-        icon: ,
+        icon: expInfo.medCSLab,
         icon_background: "#383E56",
-        date: "",
+        date: "May 2025 - Present",
         points: [
-            "",
-            "",
-            "",
+            "Managing relations with nearby programs",
+            "Attending weekly meetings",
+            "Communicating with CSI and other club/lab management programs",
 
         ],
     },
@@ -158,13 +148,13 @@ const ExperienceInfo = [
     {
         title: "Teaching Assistant",
         company_name: "Khoury College of Computer Sciences",
-        icon: ,
+        icon: expInfo.khouryCollege,
         icon_background: "#383E56",
-        date: "",
+        date: "Sep 2024 - April 2025",
         points: [
-            "",
-            "",
-            "",
+            "Teaching Assistant for Foundations of Data Science (DS 3000)",
+            "Assisted in grading assignments, labs, and tests across four sections consisting of 305 students altogether (Fall 2024)",
+            "Proctored labs and tests and conducted multiple office hours sessions a week to aid students",
 
         ],
     },
@@ -172,28 +162,26 @@ const ExperienceInfo = [
     {
         title: "IT Application Business Analyst Developer Co-op",
         company_name: "MassDOT",
-        icon: ,
+        icon: expInfo.massDOT,
         icon_background: "#383E56",
-        date: "",
+        date: "Jul 2025 - Present",
         points: [
-            "",
-            "",
-            "",
-
+            "TBD!",
+            "TBD!",
+            "TBD!",
         ],
     },
 
     {
         title: "Software Engineer",
         company_name: "ScribeSense - The Future of Clinical Documentation",
-        icon: ,
+        icon: expInfo.scribeSense,
         icon_background: "#383E56",
-        date: "",
+        date: "Jul 2025 - Present",
         points: [
-            "",
-            "",
-            "",
-
+            "TBD!",
+            "TBD!",
+            "TBD!",
         ],
     },
 
