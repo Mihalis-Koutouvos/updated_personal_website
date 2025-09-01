8ebff8caa2c8ee500180459fbbd822dedd7bb882
Mihalis Koutouvos <mihaliskoutouvos@gmail.com>
2025-09-01 10:48:00 -0400

    Removed the page loader material because it did not flow well with the rest of my site

--- updated_personal_website/src/App.js
+++ updated_personal_website/src/App.js
@@ -8,37 +8,19 @@ import Projects from "./Projects";
 import Interests from "./Interests";
 import Contact from "./Contact";
 import Footer from "./Footer";
-import PageLoader from './PageLoader';
-import {useState, useEffect} from 'react';
 
 function App() {
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => { 
-    const timer = setTimeout( () => {
-      setLoading(false);
-    }, 2000);
-    
-    return () => clearTimeout(timer);
- }, []);
-
- if (loading) {
-    return <PageLoader />;
-  }
-
-
+  
   return (
     <div className="App">
       <Navbar />
-      <About /> 
+      <About />
       <Experiences />
       <Skills />
       <Projects />
       <Interests />
       <Contact />
       <Footer />
-
-      
     </div>
   );
 }
